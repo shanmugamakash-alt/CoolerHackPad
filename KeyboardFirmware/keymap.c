341bd350536e34b6ec72e9d2e42b05daaabeb935
Shanmugamakash Rajaravisankar <shanmugamakash.rajaravi@gmail.com>
2026-09-16 21:51:17 -0500

    Add initial keymap configuration for keyboard firmware

---
+++ KeyboardFirmware/keymap.c
@@ -0,0 +1,17 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+#define _BL 0
+#define _FL 1
+#define _CL 2
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* Keymap _BL: Base Layer (Default Layer)
+   */
+[_BL] = LAYOUT(
+  KC_SYSTEM_POWER, KC_SYSTEM_SLEEP, KC_MEDIA_PLAY_PAUSE
+  KC_AUDIO_VOL_UP, KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN),
+
+
+};
